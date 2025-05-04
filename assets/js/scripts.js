96ff11904500753e504a23fa42e4345d4c78bf76
Mahdi Mohammadi <mahdinyxraven721@gmail.com>
2025-05-05 01:22:03 +0330

    Section 2 Fertig

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,16 @@
 'use strict'
 
+
+const hamburgerMenu = document.querySelector('#hamburger')
+const nav = document.querySelector('nav')
+const hamburgerMenuContent = document.querySelector('#hamburger-menu-content')
+const mobileMenu = document.querySelector('#mobile-menu')
+
+hamburgerMenu.addEventListener('click', () => {
+    nav.classList.toggle('nav-hamburger-active')
+    hamburgerMenuContent.classList.toggle('hamburger-menu-content-active')
+    
+    setTimeout(() => {
+        mobileMenu.classList.toggle('mobile-menu-active')
+    }, 600)
+})
