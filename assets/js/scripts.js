12ac675c2e636125459a5592b5606983ed2e70f0
Mahdi Mohammadi <mahdinyxraven721@gmail.com>
2025-05-07 00:30:40 +0330

    section 3 finished

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,18 @@
-'use strict'
+"use strict";
 
+// Hamburger Menu
 
-const hamburgerMenu = document.querySelector('#hamburger')
-const nav = document.querySelector('nav')
-const hamburgerMenuContent = document.querySelector('#hamburger-menu-content')
-const mobileMenu = document.querySelector('#mobile-menu')
+const hamburgerMenu = document.querySelector("#hamburger");
+const nav = document.querySelector("nav");
+const hamburgerMenuContent = document.querySelector("#hamburger-menu-content");
+const mobileMenu = document.querySelector("#mobile-menu");
 
-hamburgerMenu.addEventListener('click', () => {
-    nav.classList.toggle('nav-hamburger-active')
-    hamburgerMenuContent.classList.toggle('hamburger-menu-content-active')
-    
-    setTimeout(() => {
-        mobileMenu.classList.toggle('mobile-menu-active')
-    }, 600)
-})
+hamburgerMenu.addEventListener("click", () => {
+  nav.classList.toggle("nav-hamburger-active");
+  hamburgerMenuContent.classList.toggle("hamburger-menu-content-active");
+
+  setTimeout(() => {
+    mobileMenu.classList.toggle("mobile-menu-active");
+  }, 600);
+});
+
